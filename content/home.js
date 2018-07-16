71dcde536b897c6fc466f0c583fe7fc998bd947e
Daniel Cook <dmc3152@yahoo.com>
2018-07-15 22:44:01 -0700

    first commit - week 2

---
+++ content/home.js
@@ -0,0 +1,21 @@
+$(function () {
+  var tmpl;   // Main template HTML
+  var tdata = {};  // JSON data object that feeds the template
+  // Initialize page
+  var initPage = function () {
+    // Load the HTML template
+    $.get("/templates/home.html", function (d) {                     // 1
+      tmpl = d;
+    });
+    // Retrieve the server data and then initialize the page
+    $.getJSON("/albums.json", function (d) {                       // 2
+      $.extend(tdata, d.data);
+    });
+    // When AJAX calls are complete parse the template
+    // replacing mustache tags with vars
+    $(document).ajaxStop(function () {
+      var renderedPage = Mustache.to_html(tmpl, tdata);          // 3
+      $("body").html(renderedPage);
+    })
+  }();
+});
